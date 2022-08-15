00c99207beb8f06f525cc6cddef84fe13e058df8
Mohsen <91451917+mohsenahmadi2003@users.noreply.github.com>
2022-08-15 09:15:03 +0430

    Morse

---
+++ morse/__init__.py
@@ -0,0 +1,3 @@
+from morse.morse import morse_encrypt, morse_decrypt
+
+__all__ = ['morse_encrypt', 'morse_decrypt']
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="Morse",
+    version="0.1",
+    description="Using this module, Morse code can be created for encryption and Morse code can also be decoded.",
+    url="https://github.com/mohsenahmadi2003/Morse.git",
+    author="Mohsen Ahmadi",
+    author_email="MohsenAhmadi2003@yahoo.com",
+    license="MIT",
+    packages=["morse"],
+    zip_safe=False
+)
